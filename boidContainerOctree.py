64127d20eecb56059ab15112988e3512ca0c2126
Christian Obereder <christian.obereder1@gmail.com>
2022-12-10 13:27:17 +0100

    WIP: added working octree boid-container that still requires some improvements (e.g restructuring instead of re-creating the octree after each step)

---
+++ boidContainerOctree.py
@@ -0,0 +1,45 @@
+import open3d as o3d
+import numpy as np
+from data_structures.octree import Octree, OctNode
+
+class BoidContainerOctree:
+    def __init__(self, worldSize = 2.1, origin=(0.0, 0.0, 0.0), max_type="nodes", max_value=10):
+        self.container = Octree(worldSize, origin, max_type, max_value)
+        self.blist = []
+
+    def add(self, boid):
+        self.container.insertNode(boid.pos, boid)
+        self.blist.append(boid)
+
+    def getNeighborhood(self, boid_i, radius_detection, radius_collision):
+        return self.container.findNeighborhoodBoids(boid_i.pos, radius_detection), \
+               self.container.findNeighborhoodBoids(boid_i.pos, radius_collision)
+
+    #Rebuilds the tree after each step for now, will be updated to doing only the neccessary
+    #re-structuring
+    def step(self):
+        for boid in self.blist:
+            boid.step(self.getNeighborhood)
+        self.container= Octree(2.1, (0.0, 0.0, 0.0), "nodes", 10)
+        for boid in self.blist:
+            self.container.insertNode(boid.pos, boid)
+
+
+
+
+    # ----------------- additional methods mainly for debugging -----------------
+
+    def getNeighborhoodNode(self, boid_i, radius_detection, radius_collision):
+        return self.container.findNeighborhoodNode(boid_i.pos, radius_detection, radius_collision)
+
+    def iterate(self):
+        for i, x in enumerate(self.container.iterateDepthFirst()):
+            print(i, ":", x)
+
+    def find(self, boid):
+        return self.container.findPosition(boid.pos)
+
+    def getRoot(self):
+        return self.container.root
+
+
--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import numpy as np
 from boidContainerList import BoidContainerList
+from boidContainerOctree import BoidContainerOctree
 from boid import Boid
+import time
 
 
 if __name__ == '__main__':
     N = 500
-    boidContainer = BoidContainerList()
+    boidContainerList = BoidContainerList()
+    boidContainerOctree = BoidContainerOctree()
     # Set static values of the boids like this: Boid.l1 = value (set to values from Ex. description per default)
 
     for i in range(N):
         pos = np.random.uniform(-1, 1, 3)
         velocity = np.array([0,0,0])
         boid = Boid(pos, velocity)
-        boidContainer.add(boid)
+        boidContainerList.add(boid)
+        boidContainerOctree.add(boid)
+
+    start = time.time()
+    for i in range(10):
+        boidContainerList.step()
+    end = time.time()
+    print("list: "  + str(end - start))
+
+    start = time.time()
+    for i in range(10):
+        boidContainerOctree.step()
+    end = time.time()
+    print("octree: "  + str(end - start))
+
 
     #while(True):
         #boidContainer.step()
