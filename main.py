7e9f4de52be1d75ac33adaed0ad9c9f9d69c9d2e
student1083 <e11771083@student.tuwien.ac.at>
2022-12-11 12:23:54 +0100

    Added Simulation basis
    Added Simulation using mayavi, can be a bit glitchy and without full color scheme.

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import numpy as np
+from mayavi import mlab
 from boidContainerList import BoidContainerList
 from boid import Boid
 
 
+def get_vis_data(boidContainer, N):
+    pos = np.zeros((3,N))
+    vel = np.zeros((3,N))
+    for i in range(N):
+        pos[:,i] = boidContainer.container[i].pos
+        vel[:,i] = boidContainer.container[i].velocity
+    return pos, vel
+        
 if __name__ == '__main__':
     N = 500
     boidContainer = BoidContainerList()
@@ -11,12 +20,41 @@ if __name__ == '__main__':
     for i in range(N):
         pos = np.random.uniform(-1, 1, 3)
         velocity = np.array([0,0,0])
+        #velocity = np.random.uniform(-0.015,0.015,3)   # Test case
+        """
+        # An Error occurs when running this starting configuration, coming from the vector_avoidColission function
+        if (N%2 == 0):
+            velocity = np.array([0,0.001,0])
+        else:
+            velocity = np.array([0,0,0.001])
+        """
         boid = Boid(pos, velocity)
         boidContainer.add(boid)
 
-    #while(True):
-        #boidContainer.step()
-        #visualize
 
+    # Set the animation framework and starting frame
+    pos, vel = get_vis_data(boidContainer, N)
+    fig = mlab.figure(size=(1600,1600))
+    s = mlab.quiver3d(pos[0,:], pos[1,:], pos[2,:], vel[0,:], vel[1,:], vel[2,:],line_width=4.0,scale_factor = 2, scale_mode = 'vector', \
+                      colormap='coolwarm',mode='2darrow',figure=fig, vmin=0, vmax=0.03)
+    mlab.axes(figure=fig, ranges = [-1,1,-1,1,-1,1])
+    ms = s.mlab_source
+    delayer = 20        # Miliseconds delay between animation runs, hard lower limit is 10ms
+    its = 100            # Iterations of the Simulation
 
+    @mlab.animate(delay = delayer)
+    def animate_loop():
+        for it in range(its):
+            boidContainer.step()
+            print("Step nr:",it+1)      # Debugging purpose
+            pos, vel = get_vis_data(boidContainer, N)
+            ms.reset(x=pos[0,:], y=pos[1,:], z=pos[2,:], u=vel[0,:], v=vel[1,:], w=vel[2,:])    # Reset data, avoids redrawing canvas
+            yield
+    animate_loop()
+    mlab.show()
 
+    
+    
+    
+
+    
