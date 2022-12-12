9d9dc9a683e92c9ba06df8ab61fe1527ae69f528
maximilianvie <maximilian.vie@gmail.com>
2022-12-12 16:07:25 +0100

    fixed several bugs

--- spatialHashtable.py
+++ spatialHashtable.py
@@ -1,31 +1,30 @@
 import numpy as np
 
+
 class spatialHashtable:
 
-    def __init__(self, N, cells_per_dim = 32):
+    def __init__(self, N, cells_per_dim=32):
 
         # cells_per_dimension
         self.cpd = 32
-        number_grid = cells_per_dim**3
+        number_grid = cells_per_dim ** 3
 
         # object index
-        object_index = np.array((N,2))
+        object_index = np.array((N, 2))
 
         # hash table
-        #hash_table = np.array((N,1))
+        # hash_table = np.array((N,1))
 
         self.hashtable = {}
 
         # pivot table
         pivot_table = np.array((number_grid, 4))
 
-
     # adds a boid to the datastructure
     def add(self, boid):
 
         index = self.getCell(boid.pos[0], boid.pos[1], boid.pos[2])
         self.add_values_in_dict(index, boid)
-
 
     def add_values_in_dict(self, key, boid):
         ''' Append multiple values to a key in
@@ -36,71 +35,62 @@ class spatialHashtable:
 
         self.hashtable[key].append(boid)
 
-
-
     def getNeighborhood(self, boid, radius):
 
-        # TODO - transform to coordinate system with 0,0,0 origin
+        x, y, z = boid.pos[0], boid.pos[1], boid.pos[2]
 
         rad = radius
 
-        #get the cells:
-        c1 = self.getCell(boid)
+        # get the cells:
+        x_minus = self.getCell(max(x - rad, -1), y, z)
+        x_plus = self.getCell(min(x + rad, 1), y, z)
 
-        x_minus = self.getCell(boid.pos[0] - rad, boid.pos[1], boid.pos[2])
-        x_plus = self.getCell(boid.pos[0] + rad, boid.pos[1], boid.pos[2])
+        y_minus = self.getCell(x, max(y - rad, -1), z)
+        y_plus = self.getCell(x, min(y + rad, 1), z)
 
-        y_minus = self.getCell(boid.pos[0], boid.pos[1] - rad, boid.pos[2])
-        y_plus =  self.getCell(boid.pos[0], boid.pos[1] + rad, boid.pos[2])
+        z_minus = self.getCell(x, y, max(z - rad, -1))
+        z_plus = self.getCell(x, y, min(z + rad, 1))
 
-        z_minus = self.getCell(boid.pos[0], boid.pos[1], boid.pos[2] - rad)
-        z_plus = self.getCell(boid.pos[0], boid.pos[1], boid.pos[2] + rad)
+        x_cell_steps = int(x_plus - x_minus)
+        y_cell_steps = int((y_plus - y_minus) / self.cpd)
+        z_cell_steps = int((z_plus - z_minus) / self.cpd ** 2)
 
-        x_cell_steps = x_plus - x_minus
-        y_cell_steps = (y_plus - y_minus)/self.cpd
-        z_cell_steps = (z_plus - z_minus)/self.cpd**2
 
         # get origin
-        xyz_minus = self.getCell(boid.pos[0] - rad, boid.pos[1] - rad, boid.pos[2] - rad)
+        xyz_minus = self.getCell(max(x - rad, -1), max(y - rad, -1), max(z - rad, -1))
 
         cell_list = list()
 
-        for i in range(0,x_cell_steps + 1):
+        for i in range(0, x_cell_steps + 1):
 
             # add on x scale
             current_cell = xyz_minus + i
 
             cell_list.append(current_cell)
 
-            #for the case there are no steps in y direction #and that base
+            # for the case there are no steps in y direction #and that base
 
             for k in range(0, z_cell_steps):
-
-                current_cell = current_cell + self.cpd**2
-                cell_list.append(current_cell)
+                tmp = current_cell + self.cpd ** 2
+                cell_list.append(tmp)
 
             for j in range(0, y_cell_steps):
-
                 current_cell = current_cell + self.cpd
                 cell_list.append(current_cell)
 
                 for k in range(0, z_cell_steps):
-
-                    current_cell = current_cell + self.cpd**2
+                    current_cell = current_cell + self.cpd ** 2
                     cell_list.append(current_cell)
 
         return cell_list
 
-
-    #def step(self):
-
-
+    # def step(self):
 
     # this is the hash function - a cell is computed for the coordinates of a boid
     # the index of the cell is returned
     def getCell(self, x, y, z):
 
-        x,y,z = self.transform_coord(x,y,z)
+        x, y, z = self.transform_coord(x, y, z)
 
         # computing a fitting modulo operator
         mod_op = 2 / self.cpd
@@ -111,37 +101,24 @@ class spatialHashtable:
         if x_cell >= self.cpd:
             x_cell = self.cpd - 1
 
-
         y_cell = y // mod_op
 
         if y_cell >= self.cpd:
             y_cell = self.cpd - 1
-
 
         z_cell = z // mod_op
 
         if z_cell >= self.cpd:
             z_cell = self.cpd - 1
 
+        final_cell = x_cell + y_cell * self.cpd + z_cell * self.cpd ** 2
 
-        final_cell = x_cell + y_cell*self.cpd + z_cell*self.cpd**2
+        return int(final_cell)
 
-        return final_cell
-
-
-    def transform_coord(self, x,y,z):
+    def transform_coord(self, x, y, z):
 
         x = x + 1
         y = y + 1
         z = z + 1
 
         return x, y, z
-
-
-
-
-
-
-
-
-
--- test_hashtable.py
+++ test_hashtable.py
@@ -6,20 +6,86 @@ from spatialHashtable import spatialHashtable
 if __name__ == '__main__':
     N = 10
 
-
     table = spatialHashtable(10, 32)
-
 
 
     # Set static values of the boids like this: Boid.l1 = value (set to values from Ex. description per default)
 
-    for i in range(N):
-        pos = np.random.uniform(-1, 1, 3)
-        velocity = np.array([0,0,0])
+    for i in range(1):
+
+
+        pos = np.array([-1, -1, -1])
+        velocity = np.array([0, 0, 0])
+        boid = Boid(pos, velocity)
+
+        table.add(boid)
+        table.getNeighborhood(boid, 0.1)
+
+        pos = np.array([0, -1, -1])
+        velocity = np.array([0, 0, 0])
+        boid = Boid(pos, velocity)
+
+        table.add(boid)
+        table.getNeighborhood(boid, 0.1)
+
+        pos = np.array([1, -1, -1])
+        velocity = np.array([0, 0, 0])
         boid = Boid(pos, velocity)
 
 
         table.add(boid)
+        table.getNeighborhood(boid, 0.1)
+
+        pos = np.array([-0.5, -1, -1])
+        velocity = np.array([0, 0, 0])
+        boid = Boid(pos, velocity)
+
+        table.add(boid)
+        table.getNeighborhood(boid, 0.1)
+
+        pos = np.array([0.5, -1, -1])
+        velocity = np.array([0, 0, 0])
+        boid = Boid(pos, velocity)
+
+        table.add(boid)
+        table.getNeighborhood(boid, 0.1)
+
+        pos = np.array([-1, 0, -1])
+        velocity = np.array([0, 0, 0])
+        boid = Boid(pos, velocity)
+
+        table.add(boid)
+        table.getNeighborhood(boid, 0.1)
+        
+
+
+        pos = np.array([-1, 0.3, -1])
+        velocity = np.array([0, 0, 0])
+        boid = Boid(pos, velocity)
+
+        table.add(boid)
+        table.getNeighborhood(boid, 0.1)
+
+        pos = np.array([1, 1, -1])
+        velocity = np.array([0, 0, 0])
+        boid = Boid(pos, velocity)
+
+        table.add(boid)
+        table.getNeighborhood(boid, 0.1)
+
+        pos = np.array([1, 1, 1])
+        velocity = np.array([0, 0, 0])
+        boid = Boid(pos, velocity)
+
+        table.add(boid)
+        table.getNeighborhood(boid, 0.1)
+
+        pos = np.array([1, 1, 1])
+        velocity = np.array([0, 0, 0])
+        boid = Boid(pos, velocity)
+
+        table.add(boid)
+        table.getNeighborhood(boid, 0.1)
 
     #while(True):
         #boidContainer.step()
